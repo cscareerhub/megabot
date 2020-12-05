f58f59679c515eb8288e70f2a22fbebb4564996c
brendacs <brenda.zhang2@gmail.com>
2020-12-05 14:46:17 -0800

    Set up mongo and client startup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import client, { BOT_TOKEN, MONGODB } from './constants';
+
+mongoose
+  .connect(MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => client.logger.info('DB is connected'))
+  .catch((err) => client.logger.error(err));
+
+client
+  .on('ready', () => client.logger.info('Bot is connected'))
+  .on('disconnect', () => client.logger.warn('Bot is disconnecting'))
+  .on('reconnecting', () => client.logger.info('Bot reconnecting'))
+  .on('error', (e) => client.logger.log(e, 'error'))
+  .on('warn', (info) => client.logger.log(info, 'warn'));
+
+client.login(BOT_TOKEN);
+
+process.on('unhandledRejection', (err) => {
+  client.logger.error(err);
+});
