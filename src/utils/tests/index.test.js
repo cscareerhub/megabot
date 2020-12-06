a384a4ae8d28334dcef9ba109465f05db019a54f
brendacs <brenda.zhang2@gmail.com>
2020-12-05 19:52:52 -0800

    Add more tests

--- src/utils/tests/index.test.js
+++ src/utils/tests/index.test.js
@@ -1,5 +1,16 @@
 import { parseMessage } from '../index';
 
-test('parseMessage', () => {
-  expect(parseMessage('++faq')).toEqual({ cmd: 'faq', subCmd: undefined });
+describe('parseMessage', () => {
+  test('returns cmd when valid', () => {
+    expect(parseMessage('++faq')).toEqual({ cmd: 'faq', subCmd: undefined });
+    expect(parseMessage('++pins')).toEqual({ cmd: 'pins', subCmd: undefined });
+  });
+
+  test('returns subcmd when present', () => {
+    expect(parseMessage('++faq LC')).toEqual({ cmd: 'faq', subCmd: 'LC' });
+  });
+
+  test('undefined when cmd invalid', () => {
+    expect(parseMessage('++invalid')).toEqual(undefined);
+  });
 });
