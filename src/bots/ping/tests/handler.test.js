c25df32ed0046ec938334efa77879f85ec6728c3
Jack Wherry <jack@snazz.xyz>
2020-12-06 19:24:07 -0600

    Add explainer comment for unit test hack

--- src/bots/ping/tests/handler.test.js
+++ src/bots/ping/tests/handler.test.js
@@ -4,6 +4,7 @@ import handler from '../handler';
 describe('ping event', () => {
   test('ping handler is called when ping event is emitted', () => {
     const logSpy = jest.spyOn(client.logger, 'debug');
+    // Give handler() a msg.channel.send() since none is available in the test env
     handler({}, { channel: { send: () => {} } });
     expect(logSpy).toHaveBeenCalledWith('ping reached');
   });
