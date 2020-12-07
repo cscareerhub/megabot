7cd5c9c733cddaeea4032429e565cc7690775851
Jack Wherry <jack@snazz.xyz>
2020-12-06 18:23:32 -0600

    Separate msg from msgText

--- src/utils/dispatcher.js
+++ src/utils/dispatcher.js
@@ -2,9 +2,9 @@ import client from '../client';
 import { prefix, validCmds } from '../constants';
 
 export const parseMessage = (msg) => {
-  msg = msg.toString();
-  if (msg.substring(0, 2) === prefix) {
-    const args = msg.substring(2).split(' ');
+  msgText = msg.toString();
+  if (msgText.substring(0, 2) === prefix) {
+    const args = msgText.substring(2).split(' ');
     const cmd = args[0];
     if (validCmds.includes(cmd)) {
       client.emit('command', cmd, args.splice(1), msg);
