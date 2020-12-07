5654609d0d520e80e8e5311282272cdecd927905
brendacs <brenda.zhang2@gmail.com>
2020-12-07 10:02:02 -0800

    Clean up code

--- src/utils/dispatcher.js
+++ src/utils/dispatcher.js
@@ -2,16 +2,10 @@ import client from '../client';
 
 export const parseMessage = (message) => {
   client.message = message;
-  const messageText = message.toString();
-  if (messageText.substring(0, 2) === client.prefix) {
-    const args = messageText.substring(2).split(' ');
-    const command = args[0];
+  if (message.content.startsWith(client.prefix)) {
+    const command = message.content.substring(2).split(' ')[0];
     if (client.commands.includes(command)) {
-      client.emit('command', command, args.splice(1));
+      client.emit(command);
     }
   }
-};
-
-export const dispatchCmd = (command, args) => {
-  client.emit(command, args);
 };
--- src/utils/tests/dispatcher.test.js
+++ src/utils/tests/dispatcher.test.js
@@ -1,14 +1,14 @@
 import client from '../../client';
-import { dispatchCmd, parseMessage } from '../dispatcher';
+import { parseMessage } from '../dispatcher';
 
 describe('parseMessage', () => {
   test('client emits command event when command is valid', () => {
     const emitterSpy = jest.spyOn(client, 'emit');
     parseMessage('++faq');
-    expect(emitterSpy).toHaveBeenCalledWith('command', 'faq', []);
+    expect(emitterSpy).toHaveBeenCalledWith('faq');
 
     parseMessage('++pins list');
-    expect(emitterSpy).toHaveBeenCalledWith('command', 'pins', ['list']);
+    expect(emitterSpy).toHaveBeenCalledWith('pins');
   });
 
   test('client does not emit command event when command is not valid', () => {
@@ -21,25 +21,5 @@ describe('parseMessage', () => {
     const emitterSpy = jest.spyOn(client, 'emit');
     parseMessage('+faq');
     expect(emitterSpy).not.toHaveBeenCalled();
-  });
-});
-
-describe('dispatchCmd', () => {
-  test('dispatches event with empty array', () => {
-    const emitterSpy = jest.spyOn(client, 'emit');
-    dispatchCmd('faq', [], '++faq');
-    expect(emitterSpy).toHaveBeenCalledWith('faq', []);
-  });
-
-  test('dispatches faq event and args', () => {
-    const emitterSpy = jest.spyOn(client, 'emit');
-    dispatchCmd('faq', ['LC'], '++faq LC');
-    expect(emitterSpy).toHaveBeenCalledWith('faq', ['LC']);
-  });
-
-  test('dispatches pins event and args', () => {
-    const emitterSpy = jest.spyOn(client, 'emit');
-    dispatchCmd('pins', ['list'], '++pins list');
-    expect(emitterSpy).toHaveBeenCalledWith('pins', ['list']);
   });
 });
