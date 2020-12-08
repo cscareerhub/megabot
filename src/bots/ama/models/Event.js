8091da3759908bbb19423e4227aa586c387fb7d2
nikmanG <ngilevskiy@gmail.com>
2020-12-07 23:39:40 -0500

    modify events a bit

--- src/bots/ama/models/Event.js
+++ src/bots/ama/models/Event.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const EventSchema = mongoose.Schema({
     title: String,
     date: Date
-}, {collections: 'Event'});
+}, {collections: 'Event', autoCreate: true});
 
 let EventModel = mongoose.model('Event', EventSchema);
 
--- src/bots/ama/tests/addEvent.test.js
+++ src/bots/ama/tests/addEvent.test.js
@@ -6,37 +6,35 @@ import mongoose from 'mongoose';
 import "babel-polyfill"
 
 describe('adding Event', () => {
-  test('ama creates an event if valid information provided', () => {  
+  test('ama creates an event if valid information provided', async () => {  
     const logSpy = jest.spyOn(client.logger, 'debug');
 
     addEvent.handle(['2020-04-20', 'Birthday', 'Party']);
 
-    setImmediate(() => {
-      EventModel.find({}).then(results => {
-        expect(results.length).toEqual(1);
-        expect(results[0].title).toEqual('Birthday Party');
-        expect(results[0].date.toDateString()).toEqual('Mon Apr 20 2020');
-      });   
-    }); 
+    await new Promise(setImmediate);
 
+    EventModel.find({}).then(results => {
+      expect(results.length).toEqual(1);
+      expect(results[0].title).toEqual('Birthday Party');
+      expect(results[0].date.toDateString()).toEqual('Mon Apr 20 2020');
+    });   
+    
     // This fails when it shouldn't
     // expect(client.message.channel.send).toHaveBeenCalledWith('Following event has been created:\n```\nEvent title: Birthday Party\nDate: Mon Apr 20 2020\n```');
   });
 
-  test('ama does not create event with invalid day', () => {  
+  test('ama does not create event with invalid day', async () => {  
     addEvent.handle(['2020-1011', 'Birthday', 'Party']);
 
-    // await new Promise(setImmediate);
+    await new Promise(setImmediate);
 
-    setImmediate(() => {
-      EventModel.find({}).then(results => {
-        expect(results.length).toEqual(0);
-      });  
+    EventModel.find({}).then(results => {
+      expect(results.length).toEqual(0);
     });  
   });
 
   test('ama does not create event with insufficient information', async () => {  
-    await addEvent.handle(['2020-10-11']);
+    addEvent.handle(['2020-10-11']);
 
     await new Promise(setImmediate);
 
@@ -62,6 +60,8 @@ describe('adding Event', () => {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
+
+    await EventModel.find({});
   });
 
   afterAll(async () => {
