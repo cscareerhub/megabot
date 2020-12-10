ceee588ebb441d9cba2e0c04b4ee465322785fe4
nikmanG <ngilevskiy@gmail.com>
2020-12-09 22:55:59 -0500

    fix test

--- src/bots/ama/tests/listEvents.test.js
+++ src/bots/ama/tests/listEvents.test.js
@@ -10,42 +10,42 @@ describe('adding Event', () => {
   test('ama lists all events', async () => {  
     await Event({
         title: "Old 1",
-        date: new Date("2015-05-01")
+        date: new Date("2015-05-01T12:00:00Z")
     }).save();
 
     await Event({
         title: "Old 2",
-        date: new Date("2015-03-01")
+        date: new Date("2015-03-01T12:00:00Z")
     }).save();
 
     await Event({
         title: "New 1",
-        date: new Date("2125-05-01")
+        date: new Date("2125-05-01T12:00:00Z")
     }).save();
 
     await Event({
         title: "New 2",
-        date: new Date("2128-03-15")
+        date: new Date("2128-03-15T12:00:00Z")
     }).save();
 
     await Event({
         title: "New 3",
-        date: new Date("2128-04-20")
+        date: new Date("2128-04-20T12:00:00Z")
     }).save();
 
     await listEvents.handle([]);
     await EventModel.deleteMany({});
     
     expect(client.message.channel.send).toHaveBeenCalledWith(`__**Past Events**__
-Sat Feb 28 2015: Old 2
-Thu Apr 30 2015: Old 1
+Sun Mar 01 2015: Old 2
+Fri May 01 2015: Old 1
 
 __**Upcoming Event**__
-Mon Apr 30 2125: New 1
+Tue May 01 2125: New 1
 
 __**Future Events**__
-Sun Mar 14 2128: New 2
-Mon Apr 19 2128: New 3
+Mon Mar 15 2128: New 2
+Tue Apr 20 2128: New 3
 `);  
   });
 
