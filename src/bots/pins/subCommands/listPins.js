4cf722c8c162b8a5c3a588f909938328306f3e98
brendacs <brenda.zhang2@gmail.com>
2020-12-13 12:57:08 -0800

    Add unit tests

--- src/bots/pins/subCommands/listPins.js
+++ src/bots/pins/subCommands/listPins.js
@@ -1,32 +1,31 @@
 import client from '../../../client';
 
-const handler = () => {
-  const channels =
-    (client.message.guild && client.message.guild.channels) || [];
-  generatePinString(channels);
-};
+// TODO: this should be configurable
+const categories = ['Careers', 'Programming'];
 
-const generatePinString = (channels) => {
-  // TODO: this should be configurable
-  const categories = ['Careers', 'Programming'];
+const handler = () => {
+  const channels = client.message.guild?.channels || {};
   channels.cache &&
     channels.cache.each((channel) => {
       if (categories.includes(channel.name)) {
         channel.children.each((child) => {
-          child.messages &&
-            child.messages
-              .fetchPinned()
-              .then((messages) => {
-                let pins = `**#${child.name}**\n\n`;
-                messages.each((message) => {
-                  pins += message.content ? `${message.content}\n\n` : '';
-                });
-                client.message.author.send(pins);
-              })
-              .catch((err) => console.log(err));
+          generatePinString(child);
         });
       }
     });
+};
+
+const generatePinString = (channel = {}) => {
+  channel.messages
+    .fetchPinned()
+    .then((messages) => {
+      let pins = `**#${channel.name}**\n\n`;
+      messages.each((message) => {
+        pins += message.content ? `${message.content}\n\n` : '';
+      });
+      client.message.author.send(pins);
+    })
+    .catch((err) => console.log(err));
 };
 
 const listPins = {
