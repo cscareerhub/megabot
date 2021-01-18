2f5209b27ea3301297b1d61581a644856e71cc4b
brendacs <brenda.zhang2@gmail.com>
2021-01-17 18:30:03 -0800

    Update english

--- src/bots/settings/constants.js
+++ src/bots/settings/constants.js
@@ -1,5 +1,6 @@
 export const strings = {
   insufficientArguments: 'You need to supply a variable name and a new value.',
-  successfullyUpdated: (oldVar, newVar) => `Updated ${oldVar} to ${newVar}.`,
+  successfullyUpdated: (oldVar, newVar) =>
+    `The ${oldVar} has been updated to ${newVar}.`,
   variableNotFound: (arg) => `Variable not found. Available options: ${arg}`
 };
