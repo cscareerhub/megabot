949004c1f6e2a0b116434dd1d701e8c39ace1a2c
Nikita Gilevskiy <github@contactnik.com>
2021-08-02 22:40:51 -0400

    add thumbs up bot, basic version

--- src/bots/misc/index.js
+++ src/bots/misc/index.js
@@ -1,6 +1,7 @@
 import client from '../../client';
 import { commandHandler } from '../../utils';
 import lmgtfy from './subCommands/lmgtfy';
+import onEventQuestion from './events/onMessage';
 import pm from './subCommands/privateMessage';
 
 const subCommands = {
@@ -9,3 +10,12 @@ const subCommands = {
 };
 
 client.on('misc', () => commandHandler(subCommands));
+
+client.on('message', (message) => {
+  //Don't upvote commands
+  if (message.content.startsWith(client.prefix)) {
+    return;
+  }
+
+  onEventQuestion(message);
+});
